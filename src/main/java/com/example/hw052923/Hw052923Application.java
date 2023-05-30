70ccfbaf313a7f1488f8d04b83f5ed1f9d328bec
Tkakishak <tkakishakd@mail.ru>
2023-05-30 21:40:01 +0300

    start

---
+++ src/main/java/com/example/hw052923/Hw052923Application.java
@@ -0,0 +1,13 @@
+package com.example.hw052923;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw052923Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw052923Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hw052923/Hw052923ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw052923;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw052923ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
